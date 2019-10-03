254fe24c6373cea87940f86af525b7f13fe24f86
Alexandra Shkaryova <Sasha-021248@yandex.ru>
2019-10-03 12:46:34 +0300

    the first interface

--- src/LikesAnalyzer.test.js
+++ src/LikesAnalyzer.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import LikesAnalyzer from './LikesAnalyzer';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<LikesAnalyzer />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
